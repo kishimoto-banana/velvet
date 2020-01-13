f79d01c0ef406e2fe36fce1eb76d75b79eb28fca
kishimoto-banana <drehbleistift@gmail.com>
2020-01-13 22:57:36 +0900

    Add form

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+from flask import redirect
+from flask import url_for
 from flask import render_template
 
 app = Flask(__name__)
@@ -6,9 +9,18 @@ TITLE = "velvet"
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html', title=TITLE)
+def index():
+    return render_template("index.html", title=TITLE)
 
 
-if __name__ == '__main__':
-    app.run(port=18000, host='0.0.0.0')
+@app.route("/prediction", methods=["GET", "POST"])
+def prediction_hatebu():
+    if request.method == "POST":
+        url = request.form["url"]
+        return render_template("index.html", title=TITLE, url=url)
+    else:
+        return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(port=18000, host="0.0.0.0", debug=True)
