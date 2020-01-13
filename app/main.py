6dc3b9f8ea24df72213afd517c4f4ae721d82846
kishimoto-banana <drehbleistift@gmail.com>
2020-01-13 22:17:43 +0900

    Render template

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+TITLE = "velvet"
 
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return render_template('index.html', title=TITLE)
 
 
 if __name__ == '__main__':
