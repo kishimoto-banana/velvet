5ccbdf4cf820f1ea48da0bdf9192ba147216d0cb
kishimoto-banana <drehbleistift@gmail.com>
2020-01-16 22:50:22 +0900

    Err handling

--- app/const.py
+++ app/const.py
@@ -10,5 +10,27 @@ title = "velvet"
 # err
 url_regex = re.compile(r"https?://[\w/:%#\$&\?~\.=\+\-]+")
 
-# err msg
+
+class InvalidUrlError(Exception):
+    """有効なURLでは無かったときに投げるエラー"""
+
+
+class CannotGetBlogContentError(Exception):
+    """ページの情報の取得に失敗したときに投げるエラー"""
+
+
+class UnexpectedError(Exception):
+    """予期していないエラーのときに投げるエラー"""
+
+
+# error code
+invalid_url_code = 2001
+exception_code = 1000
+cannot_scrape_code = 1001
+cannot_tokenize_code = 1002
+cannot_predict_code = 1003
+
+# error msg
 invalid_url_msg = "有効なURLではありません"
+cannot_get_blog_content_msg = "ブログの情報を取得できませんでした。URLを確認してみてください。"
+unexpected_msg = "予期していないエラーが起きました。ちょっと諦めてください…"
