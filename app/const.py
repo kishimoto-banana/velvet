b40c7fe4b6bf465408344449ee77ae3bdc581362
kishimoto-banana <drehbleistift@gmail.com>
2020-01-16 21:54:25 +0900

    Request hatebu api

--- app/const.py
+++ app/const.py
@@ -1,7 +1,14 @@
 import re
 
+# api
+config_path = "/app/config.yml"
+hatebu_prediction_path = "/v1/prediction"
+
+# meta
 title = "velvet"
 
+# err
 url_regex = re.compile(r"https?://[\w/:%#\$&\?~\.=\+\-]+")
 
+# err msg
 invalid_url_msg = "有効なURLではありません"
--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import yaml
+import requests
 from flask import Flask
 from flask import request
 from flask import redirect
@@ -5,10 +7,15 @@ from flask import url_for
 from flask import render_template
 
 from const import (
+    config_path,
     title,
+    hatebu_prediction_path,
     url_regex,
     invalid_url_msg,
 )
+
+with open(config_path, "r", encoding="utf-8") as f:
+    config = yaml.load(stream=f, Loader=yaml.SafeLoader)
 
 app = Flask(__name__)
 
@@ -18,6 +25,15 @@ def valid_url(url: str) -> bool:
         return True
     else:
         return False
+
+
+def get_hatebu(url: str) -> dict:
+    address = config["velvet-api"]["address"]
+    request_url = f"http://{address}{hatebu_prediction_path}?url={url}"
+    res = requests.get(request_url)
+    hatebu_info = res.json()
+
+    return hatebu_info
 
 
 @app.route('/')
@@ -32,7 +48,12 @@ def prediction_hatebu():
         return render_template("index.html",
                                title=title,
                                err_msg=invalid_url_msg)
-    return render_template("index.html", title=title, url=url)
+    hatebu_info = get_hatebu(url)
+
+    return render_template("index.html",
+                           title=title,
+                           is_hatebu=hatebu_info["is_hatebu"],
+                           hatebu_num=hatebu_info["hatebu_num"])
 
 
 if __name__ == "__main__":
